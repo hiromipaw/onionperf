f93b03bef1b4617646b01cc738ad02590ae82f7b
Silvia/Hiro <hiro@torproject.org>
2022-06-23 09:22:55 +0200

    Add filter for exits and tests

--- onionperf/tests/test_filtering.py
+++ onionperf/tests/test_filtering.py
@@ -66,3 +66,31 @@ def test_exclude_guards():
     assert_false(measurements['filtered_out'])
     guard_measurements = json_content['data']['op-hk6a']['tor']['circuits'][guard_circuit_id]
     assert_true(guard_measurements['filtered_out'])
+
+def test_exclude_exits():
+    exit = "7AFC157269130BCF36BCCAC0F2DAA0685E70D40D"
+    exit_circuit_id = "177926"
+    fingerprint = "9E639C45F9AF3C9096B4BFA34DEACF1B1DA41766"
+    circuit_id = "183977"
+    onion = "6D0D610D63D33584308CA672484DA699FF26A2A3"
+    onion_circuit_id = "177929"
+    filtering = Filtering()
+    filtering.exits = True
+    filtering.fingerprints_to_exclude = []
+    filtering.fingerprints_to_exclude.append(exit)
+    filtering.fingerprints_to_exclude.append(fingerprint)
+    filtering.fingerprints_to_exclude.append(onion)
+    filtering.fingerprints_to_exclude_path = absolute_data_path("analyses/guards-fingerprints.txt")
+    input_path = absolute_data_path("analyses/2021-06-01.op-hk6a.onionperf.analysis_no_filters.json.xz")
+    output_dir = absolute_data_path("analyses/")
+    output_file = "filtered-exits-onionperf.analysis.json.xz"
+    filtering.apply_filters(input_path=input_path, output_dir=output_dir, output_file=output_file)
+    xz_file = lzma.open(absolute_data_path("analyses/filtered-exits-onionperf.analysis.json.xz"))
+    json_content = json.load(xz_file)
+    assert_equals(json_content["filters"]["tor/circuits"][0]["name"], "exclude_fingerprints")
+    measurements = json_content['data']['op-hk6a']['tor']['circuits'][circuit_id]
+    assert_false(measurements['filtered_out'])
+    exit_measurements = json_content['data']['op-hk6a']['tor']['circuits'][exit_circuit_id]
+    assert_true(exit_measurements['filtered_out'])
+    onion_measurements = json_content['data']['op-hk6a']['tor']['circuits'][onion_circuit_id]
+    assert_false(onion_measurements['filtered_out'])
