2069391c0147560f3a3812866dbfe9c96186fab8
Silvia/Hiro <hiro@torproject.org>
2022-10-30 15:14:25 +0100

    Fix setuppy bug.

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-from setuptools import find_packages, setup
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    install_requires = f.readlines()
 
 setup(name='OnionPerf',
       version='1.0',
       description='A utility to monitor, measure, analyze, and visualize the performance of Tor and Onion Services',
       author='Rob Jansen',
-      url='https://gitlab.torproject.org/tpo/network-health/metrics/onionperf',
-      packages=find_packages(where="onionperf"),
-      package_dir={"": "onionperf"},
+      url='https://gitlab.torproject.org/tpo/network-health/metrics/onionperf/',
+      packages=['onionperf'],
+      scripts=['onionperf/onionperf'],
       install_requires=[
         "lxml",
         "matplotlib",
@@ -20,6 +23,5 @@ setup(name='OnionPerf',
         "stem >= 1.7.0",
         "tgentools @ git+https://github.com/shadow/tgen.git@main#egg=tgentools&subdirectory=tools",
         "requests"
-      ],
-      scripts=['onionperf/onionperf']
+      ]
      )
