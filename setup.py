8b44121b22c5fa883b053e52b8937d9e92e2ef91
Silvia/Hiro <hiro@torproject.org>
2022-10-30 14:13:20 +0100

    Update setup.py and requirements.txt to differentiate between dev dependencies and install dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
-
-with open('requirements.txt') as f:
-    install_requires = f.readlines()
+from setuptools import find_packages, setup
 
 setup(name='OnionPerf',
       version='1.0',
       description='A utility to monitor, measure, analyze, and visualize the performance of Tor and Onion Services',
       author='Rob Jansen',
-      url='https://github.com/robgjansen/onionperf/',
-      packages=['onionperf'],
-      scripts=['onionperf/onionperf'],
-      install_requires=install_requires
+      url='https://gitlab.torproject.org/tpo/network-health/metrics/onionperf',
+      packages=find_packages(where="onionperf"),
+      package_dir={"": "onionperf"},
+      install_requires=[
+        "lxml",
+        "matplotlib",
+        "networkx",
+        "numpy",
+        "pandas",
+        "scipy",
+        "seaborn >= 0.11",
+        "stem >= 1.7.0",
+        "tgentools @ git+https://github.com/shadow/tgen.git@main#egg=tgentools&subdirectory=tools",
+        "requests"
+      ],
+      scripts=['onionperf/onionperf']
      )
